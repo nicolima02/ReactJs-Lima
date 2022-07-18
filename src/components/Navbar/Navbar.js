1fcf4baa2611db822f434bf43b3d81162c1842ca
Nicolas <nikitolima123456789@gmail.com>
2022-07-17 21:20:37 -0300

    entrega 2

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return ( 
+        <nav> 
+            <div>
+                <h1>NeonDesign</h1>
+            </div>
+            <div>
+                <button>Retroiluminado</button>
+                <button>Neon</button>
+                <button>MDF</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
